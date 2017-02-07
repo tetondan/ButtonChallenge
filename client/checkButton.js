1f54a4e3e6cf0524859b0cf6c711959a10c9ea30
tetondan <dfrehner1@gmail.com>
2017-02-06 23:17:56 -0700

    added some features

--- client/checkButton.js
+++ client/checkButton.js
@@ -1,34 +1,34 @@
 import React, {Component} from "react"
 
 export default class CheckButton extends Component {
-	constructor(props){
-		super(props)
-		this.state = {
-			checked: props.checked
-		}
-		/*props = 
-		name: string
-		value: any
-		checked: boolean
-		label: string|element|component
-		onChange: function
-		*/
-		this.checkBoxClickHandler = this.checkBoxClickHandler.bind(this);
-	}
-	checkBoxClickHandler(){
-		this.setState((prev) => {
-			return {checked: !prev.checked}})
-	}
-	render(){
-		return (
-			<div>
-				<input type="checkbox"
-				name={this.props.name}
-				value={this.props.value}
-				checked={this.state.checked}
-				onChange={this.checkBoxClickHandler} />
-				{this.props.label}
-			</div>
-		)
-	}
+  constructor(props){
+    super(props)
+    this.state = {
+      checked: props.checked
+    }
+    /*props = 
+    name: string
+    value: any
+    checked: boolean
+    label: string|element|component
+    onChange: function
+    */
+    this.checkBoxClickHandler = this.checkBoxClickHandler.bind(this);
+  }
+  checkBoxClickHandler(){
+    this.setState((prev) => {
+      return {checked: !prev.checked}})
+  }
+  render(){
+    return (
+      <div>
+        <input type="checkbox"
+        name={this.props.name}
+        value={this.props.value}
+        checked={this.state.checked}
+        onChange={this.checkBoxClickHandler} />
+        {this.props.label}
+      </div>
+    )
+  }
 }
